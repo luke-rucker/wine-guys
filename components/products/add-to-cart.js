9aa414ecc09478784e0e375730612817166493fb
Luke Rucker <ruckerlh@gmail.com>
2021-02-05 01:39:39 +0100

    Change validation of add to cart input

--- components/products/add-to-cart.js
+++ components/products/add-to-cart.js
@@ -5,29 +5,47 @@ import { useCart } from '../../context/cart-context'
 export default function AddToCart({ product }) {
     const cart = useCart()
 
+    const [error, setError] = React.useState('')
     const [quantity, setQuantity] = React.useState(1)
     const [visible, setVisible] = React.useState(false)
 
     function handleChange({ target: { value } }) {
-        if (!value) return
-        setQuantity(parseInt(value))
+        setQuantity(value)
+    }
+
+    function validate(quantity) {
+        if (!quantity) {
+            return 'A quantity is required.'
+        }
+        if (quantity == 0) {
+            return 'A quantity more than zero is required.'
+        }
+        return
     }
 
     function handleSubmit() {
-        cart.addItem(product, quantity)
-        setVisible(true)
-        setTimeout(() => setVisible(false), 1000)
+        const error = validate(quantity)
+        setError(error)
+
+        if (!error) {
+            cart.addItem(product, quantity)
+            setVisible(true)
+            setTimeout(() => setVisible(false), 1000)
+        }
     }
 
     return (
         <>
             <Input
                 type="number"
+                pattern="[0-9]*"
+                inputmode="numeric"
                 placeholder="Quantity"
                 value={quantity}
                 min={1}
                 step={1}
                 onChange={handleChange}
+                error={!!error}
                 action={{
                     style: { backgroundColor: '#8d1111', color: '#fff' },
                     content: 'Add to Cart',
@@ -35,6 +53,11 @@ export default function AddToCart({ product }) {
                     onClick: handleSubmit,
                 }}
             />
+            {error && (
+                <div style={{ color: 'red', position: 'absolute' }}>
+                    {error}
+                </div>
+            )}
             <Transition duration={{ hide: 1000, show: 1000 }} visible={visible}>
                 <div style={{ color: 'green', position: 'absolute' }}>
                     <Icon name="check" />
